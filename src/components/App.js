d556465dd73a37c5c48c339e2d7ccc439a41b601
ubuntu <princechauhan248008@gmail.com>
2017-06-06 17:21:34 +0530

    only app.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,35 +6,37 @@ import DevTools from "mobx-react-devtools";
 import axios from "axios"
 import TopBar from "./TopBar";
 import {observable, autorun} from "mobx";
-import users from "./usernames_arr"
-import thearray from "./usernames_arr"
+//import users from "./usernames_arr"
+//import thearray from "./usernames_arr"
 
+var usernames = [];
 @inject("store")
 @observer
 export default class App extends Component {
+
     constructor(props) {
         super();
         super(props);
         this.store = this.props.store;
-        this.state = {name: [], public_repos: [], arr: []}
+        this.state = {usernames: [], public_repos: [], arr: []}
     }
 
     componentWillMount() {
         var _this = this;
-        var username = "prince-chauhan";
-        /*this.serverRequest = axios.get("https://api.github.com/search/users?q=tom+repos:%3E30+followers:%3E354").then(function (arr) {
-            _this.setState({
-				name : "the name",
-				public_repos : "1234",
-			});
-        })*/
-        const { usernames } = this.props.users
+        this.serverRequest = axios.get("https://api.github.com/search/users?q=tom+repos:%3E30+followers:%3E354").then(function (arr) {
 
-        {users.get_usernames()}
-        console.log(usernames)
+            usernames.push(arr.data.items[0].login);
+            usernames.push(arr.data.items[1].login);
+            usernames.push(arr.data.items[2].login);
+            usernames.push(arr.data.items[3].login);
+            usernames.push(arr.data.items[4].login);
+            usernames.push(arr.data.items[5].login);
+            usernames.push(arr.data.items[6].login);
+            usernames.push(arr.data.items[7].login);
+            usernames.push(arr.data.items[8].login);
+            console.log(usernames);
+        })
     }
-	
-
 
     componentDidMount() {
         this.authenticate();
@@ -44,18 +46,18 @@ export default class App extends Component {
         if (e) e.preventDefault();
         this.store.appState.authenticate();
     }
+    handleclick(username){
+        console.log(username)
+    }
 
     render() {
-        const { usernames } = this.props.users
+        var userlist = usernames.map(username => (
+            <a href="#" onClick={this.handleclick.bind(this)}>{username}</a>
+        ))
         return (
-
 			<div>
-				<h1>repos!</h1>
-
-                {this.props.users.usernames}
-				Name = {this.state.name}
-				Public repos = {this.state.public_repos}
-
+				<h1>Top GitHub users</h1>
+                <ul>{userlist}</ul>
 			</div>
         );
     }
--- src/components/usernames_arr.js
+++ src/components/usernames_arr.js
@@ -11,11 +11,11 @@ class usernames_arr{
     get_usernames() {
         var _this = this;
         this.serverRequest = axios.get("https://api.github.com/search/users?q=tom+repos:%3E30+followers:%3E354").then(function (arr) {
-           //console.log(arr.data.items[0]);
+           console.log(arr.data.items[0]);
 
             //thearray.push(arr);
-            //console.log("Users: ", arr.map(data => data.items[0].login).join(", "))
-            _this.usernames.push(String(arr.data.items[0].login));
+            console.log("Users: ", arr.map(data => data.items[0].login).join(","))
+            _this.usernames.push(String(arr.data.items[0]));
             console.log(_this.usernames.login)
 
         })
